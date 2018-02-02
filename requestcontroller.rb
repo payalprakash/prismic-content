645e768e238afb09b8e1d8dfb83eee32a8ec4424
payalprakash <payal@webscalenetworks.com>
2018-02-02 13:55:47 -0700

    add response for favicon

--- requestcontroller.rb
+++ requestcontroller.rb
@@ -6,6 +6,15 @@ class RequestController
     r = BrainRack.new
     route = r.router.route_for(env)
     if route
+      if env['PATH_INFO'] == '/favicon.ico'
+        return [
+          200,
+          { 'Content-Type' => 'text/html' },
+          [
+            "<link rel='icon' href='public/images/favicon.ico' />"
+          ]
+        ]
+      end
       response = route.execute(env)
       return response.rack_response
     else
